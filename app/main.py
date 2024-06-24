3da02212a32c3b2029be1d80f8017b496d73677e
mukaj <43552866+mukaj@users.noreply.github.com>
2024-06-24 11:59:54 -0400

    Clean comment/print statement.

--- app/main.py
+++ app/main.py
@@ -29,9 +29,6 @@ def handle_connection(conn, addr):
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     server_socket.listen()
 
