4959e573ee6b629a9f6dccba79c6d0a6385224a3
mukaj <43552866+mukaj@users.noreply.github.com>
2024-06-24 02:23:41 -0400

    Add `echo` path parsing, rewrite responses using `Response` class.

--- app/main.py
+++ app/main.py
@@ -15,9 +15,18 @@ def main():
     request: Request = Request.from_string(byte_request.decode())
 
     if request.target == "/":
-        conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+        response = Response(status=200)
+        conn.sendall(response.send_response())
+    elif request.target_paths and request.target_paths[1] == "echo":
+        message = request.target_paths[2]
+        response = Response(status=200, body=message)
+        response.headers["Content-Type"] = "text/plain"
+        response.headers["Content-Length"] = str(len(message))
+        conn.sendall(response.send_response())
     else:
-        conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+        response = Response(status=404)
+        conn.sendall(response.send_response())
+        # conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
 
 
 if __name__ == "__main__":
