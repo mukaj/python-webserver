de61e49e186bae1a4e3cddf0a59c3288a605e055
mukaj <43552866+mukaj@users.noreply.github.com>
2024-06-24 03:13:55 -0400

    Add header parsing support for `Request` class.

--- app/request.py
+++ app/request.py
@@ -1,4 +1,5 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import Dict
 
 
 @dataclass
@@ -6,6 +7,7 @@ class Request:
     method: str
     target: str
     http_version: str
+    headers: Dict[str, str] = field(default_factory=dict)
 
     @classmethod
     def from_string(cls, request_string: str) -> "Request":
@@ -18,10 +20,20 @@ class Request:
         Returns:
             Request: An instance of the Request class.
         """
-        line, headers, body, _ = request_string.split("\r\n")
-        method, target, version = line.split()
+        line_ending = request_string.find("\r\n")
+        status_line = request_string[:line_ending]
+        headers: Dict[str, str] = {}
 
-        return cls(method=method, target=target, http_version=version)
+        for header_str in request_string[line_ending + 2 :].split("\r\n"):
+            if header_str == "":
+                break
+            header, value = header_str.split(":", maxsplit=1)
+            print(header + "  " + value)
+            headers[header] = value.strip()
+
+        method, target, version = status_line.split()
+
+        return cls(method=method, target=target, http_version=version, headers=headers)
 
     @property
     def target_paths(self) -> list[str]:
